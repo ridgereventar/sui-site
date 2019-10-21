4ea76a6e4751be67c10836216ed0301a628a957f
ridge <ridge.reventar@gmail.com>
2019-10-21 08:37:35 -0400

    Created a default style guide template

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ function App() {
         </div>
 
         <div className="style-guide">
+          <h4>Color Palette</h4>
+          <hr/>
+          <div className="color-palette-container">
+            <div className="color-container" id="color1"></div>
+            <div className="color-container" id="color1"></div>
+            <div className="color-container" id="color1"></div>
+          </div>
+      
+          <h4>Typography</h4>
+          <hr/>
+          <h1>Headline Large</h1>
+          <h2>Headline Medium</h2>
+          <h3>Headline</h3>
+          <h5>Body</h5>
+          
         </div>
 
         <div id="settings-panel-right" className="settings-panel-container">
