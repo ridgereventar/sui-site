15419ae4733486b609f1f42b0038fcb02741e770
ridge <ridge.reventar@gmail.com>
2019-10-21 08:22:44 -0400

    Configured window layout and separated settings panels

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div id="header">
+      </div>
+
+      <div className="playground-window">
+        <div id="settings-panel-left" className="settings-panel-container">
+        </div>
+
+        <div className="style-guide">
+        </div>
+
+        <div id="settings-panel-right" className="settings-panel-container">
+        </div>
+      </div>
+
+      <div id="footer">
+      </div>  
     </div>
+
   );
 }
 
