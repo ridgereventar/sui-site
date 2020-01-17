c80057c4d997d574cd3fc8ef433429f4ca03ccf9
Ridge Reventar <ridge.reventar@gmail.com>
2020-01-16 23:59:24 -0500

    Created landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -14,22 +14,39 @@ class Landing extends Component {
 
         return (
             <React.Fragment>
-              <div className="landing-header">
-                <div className="landing-logo"></div>
-                <div className="landing-nav">
-                  <ul className="landing-nav-links">
-                    <li><NavLink className="nav-link" to="/explore">Explore</NavLink></li>
-                    <li><NavLink className="nav-link" to="/login">Log in</NavLink></li>
-                    <li>
-                      <NavLink className="nav-link" to="/login">
-                        <button id="signupBtn">Sign up</button>
-                      </NavLink>
-                    </li>
-
-
-                  </ul>
+              <div className="landing-container">
+                <div className="landing-header">
+                  <div className="landing-logo"></div>
+                  <div className="landing-nav">
+                    <ul className="landing-nav-links">
+                      <li><NavLink className="nav-link" to="/explore">Explore</NavLink></li>
+                      <li><NavLink className="nav-link" to="/login">Log in</NavLink></li>
+                      <li>
+                        <NavLink className="nav-link" to="/login">
+                          <button id="signupBtn">Sign up</button>
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
+              
+                <div className="welcome-container">
+                  <div className="welcome-text-container">
+                    <div className="welcome-box"></div>
+                  </div>
+                  <div className="welcome-anim-container">
+                    <div className="graphic"></div>
+                  </div>
+                </div>
+
               </div>
+
+              <div className="intro-container">
+
+              </div>
+
+
+
             </React.Fragment>
         );
     }
